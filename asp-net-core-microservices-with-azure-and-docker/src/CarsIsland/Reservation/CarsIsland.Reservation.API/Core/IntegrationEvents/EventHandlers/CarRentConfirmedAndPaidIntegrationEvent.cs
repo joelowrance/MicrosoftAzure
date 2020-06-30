e0f8fac5eb59a1cbca8b8a2eecadcae595abc668
Daniel <krzyczkowski.daniel@op.pl>
2020-06-30 19:50:25 +0200

    Add handling car rental confirmation integration event

--- asp-net-core-microservices-with-azure-and-docker/src/CarsIsland/Reservation/CarsIsland.Reservation.API/Core/IntegrationEvents/EventHandlers/CarRentConfirmedAndPaidIntegrationEvent.cs
+++ asp-net-core-microservices-with-azure-and-docker/src/CarsIsland/Reservation/CarsIsland.Reservation.API/Core/IntegrationEvents/EventHandlers/CarRentConfirmedAndPaidIntegrationEvent.cs
@@ -1,7 +1,27 @@
-﻿namespace CarsIsland.Reservation.API.Core.IntegrationEvents.Events
-{
-    public class CarRentConfirmedAndPaidIntegrationEventHandler
-    {
+﻿using CarsIsland.EventBus.Events.Interfaces;
+using CarsIsland.Reservation.Domain.Repositories.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
+namespace CarsIsland.Reservation.API.Core.IntegrationEvents.Events
+{
+    public class CarRentalConfirmedAndPaidIntegrationEventHandler : IIntegrationEventHandler
+    {
+        private readonly IReservationRepository _reservationRepository;
+        private readonly ILogger<CarRentalConfirmedAndPaidIntegrationEventHandler> _logger;
+
+        public CarRentalConfirmedAndPaidIntegrationEventHandler(
+            IReservationRepository reservationRepository,
+            ILogger<CarRentalConfirmedAndPaidIntegrationEventHandler> logger)
+        {
+            _reservationRepository = reservationRepository ?? throw new ArgumentNullException(nameof(reservationRepository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Handle(CarRentalConfirmedAndPaidIntegrationEvent @event)
+        {
+            await _reservationRepository.DeleteReservationAsync(@event.UserId);
+        }
     }
 }
