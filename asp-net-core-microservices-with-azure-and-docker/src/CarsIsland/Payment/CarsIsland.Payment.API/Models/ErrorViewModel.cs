8991318d0031126382fdf33dbc797519b8773dec
Daniel <krzyczkowski.daniel@op.pl>
2020-07-03 07:22:44 +0200

    Start implementation of payment api

---
+++ asp-net-core-microservices-with-azure-and-docker/src/CarsIsland/Payment/CarsIsland.Payment.API/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CarsIsland.Payment.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
